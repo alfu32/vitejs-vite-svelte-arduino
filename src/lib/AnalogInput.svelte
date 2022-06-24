02a5049db653825a8f4313b06f701d51f01786bc
alfu32 <ioan.alferaru@gmail.com>
2022-06-24 16:18:13 +0200

    1 file modified

--- src/lib/AnalogInput.svelte
+++ src/lib/AnalogInput.svelte
@@ -3,7 +3,7 @@
   export let id = "D0"
   let reading = 1.67
   let intvId = setInterval(
-    ()={
+    ()=>{
       reading=randomSeries(randomSeries({ min: 0, max: 3.3, granularity: 0.001 }))
     },1000
   )
@@ -22,5 +22,5 @@
   <pre style="min-width:10em;max-width:10em;display:inline-block">{human({ value: reading, unit: 'V' })}</pre>
   <br/>{reading.toFixed(6)}
 
-  <span style="display: inline-block;min-width: 3rem;max-width:3rem;text-align:right">{pinValue}%</span>
+  <span style="display: inline-block;min-width: 3rem;max-width:3rem;text-align:right">{reading}%</span>
 </div>
